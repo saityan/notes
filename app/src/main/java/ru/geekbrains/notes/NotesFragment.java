8bdf0064b2598e2193330eebc105a63d68b530f4
saityan <naminatosky@gmail.com>
2021-08-16 15:47:46 +0300

    some minor changes

--- app/src/main/java/ru/geekbrains/notes/NotesFragment.java
+++ app/src/main/java/ru/geekbrains/notes/NotesFragment.java
@@ -22,7 +22,9 @@ public class NotesFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_notes, container, false);
+
         initList(view);
+
         return view;
     }
 

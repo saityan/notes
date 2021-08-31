5d882c8c471073f28d9a05dd6dcf4d425053371c
saityan <naminatosky@gmail.com>
2021-08-31 06:29:45 +0300

    kotlin branch started

--- app/src/main/java/ru/geekbrains/notes/MainActivity.java
+++ app/src/main/java/ru/geekbrains/notes/MainActivity.java
@@ -8,7 +8,6 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;

82c89310c1f4114d21183dc70bf38428f0689e7a
saityan <naminatosky@gmail.com>
2021-08-31 07:20:20 +0300

    CardSource to Korlin

---
+++ app/src/main/java/ru/geekbrains/notes/data/CardSource.kt
@@ -0,0 +1,12 @@
+package ru.geekbrains.notes.data
+
+interface CardSource {
+    fun size(): Int
+    fun getCardData(position: Int): CardData?
+    fun init(cardsSourceResponse: CardsSourceResponse?): CardSource?
+
+    fun deleteCardData(position: Int)
+    fun updateCardData(position: Int, newCardData: CardData?)
+    fun addCardData(newCardData: CardData?)
+    fun clearCardData()
+}
